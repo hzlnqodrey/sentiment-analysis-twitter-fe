b9425691d5462903fe1e76a27bb77eab46b2ea41
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-27 03:24:28 +0700

    rick and morty api works i guess

--- app/staticprops/page.tsx
+++ app/staticprops/page.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link"
+import { use } from "react"
+import { resourceLimits } from "worker_threads"
+
+// getStaticProps in NextJS13
+async function getCharacters() {
+    return await (await fetch("https://rickandmortyapi.com/api/character")).json()
+}
+
 export default function StaticPage() {
+
+    // use the json data
+    const allCharacters = use(getCharacters())
+
     return (
-        <div>Static Page</div>
+        <div>
+            <h2>getStaticPaths and getStaticProps</h2>
+            {allCharacters?.results.map((result: any, index: number) => (
+                <ul key={result.id}>
+                    <Link href={`/staticprops/${result.name}`.replace(/\s+/g, "-").toLowerCase()}>
+                        <li>{result.name} : {result.species} | {result.status}</li>
+                    </Link>
+                </ul>
+            ))}
+        </div>
     )
 }
