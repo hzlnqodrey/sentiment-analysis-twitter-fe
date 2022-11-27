53da93a38ea78915d5e8227890a7b8ed5a1c0938
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-28 01:10:58 +0700

    change whole font to plus-jakarta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <Head />
-      <body className="wrapper sm:text-sm md:text-base lg:text-lg xl:text-xl text-lg">
+      <body className="wrapper sm:text-sm md:text-base lg:text-lg xl:text-xl text-lg font-plus-jakarta">
         <Navbar />
         {children}
       </body>
---
+++ app/wordcloud/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "wordcloud.module.css"
+
+export default function WordcloudPage() {
+
+    return (
+        <>
+            <h1 className="text-center font-bold">Hasil Word Cloud</h1>
+        </>
+    )
+}
