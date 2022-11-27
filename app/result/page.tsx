ff9be828fd86291d5ab23722fdc0e545d6babd3a
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-28 05:49:50 +0700

    table skeleton done

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -5,15 +5,36 @@ async function getData() {
     return await (await fetch("https://sentimen-analisis-twitter-api.herokuapp.com/predict", { cache: "no-store" })).json()
 }
 
-export default async function ResultPage() {
+export default function ResultPage() {
 
-    const analysisResults = use(getData())
+    // const analysisResults = use(getData())
 
     return (
         <>
-            <h1 className="text-center">Result</h1>
-            <table className={ style.mystyle }>
-
+            <h1 className="text-center mb-10">Result</h1>
+            <table className={ style.tablestyle }>
+                <thead>
+                    <tr className="text-center">
+                    <th>x</th>
+                    <th>Text_Asli</th>
+                    <th>Text_Clean</th>
+                    <th>Text_Clean_English</th>
+                    <th>Klasifikasi_NLP</th>
+                    <th>Klasifikasi_NaiveBayes</th>
+                    <th>Polaritas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="text-center">
+                        <th>0</th>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
             </table>
         </>
     )
